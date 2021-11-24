eff7686ea183ccee2c347b4404c387031425dfc1
Jacquie Tran <jacquietran@users.noreply.github.com>
2021-11-24 22:13:05 +1300

    Tidy up pbp

--- data-raw/pbp_load.R
+++ data-raw/pbp_load.R
@@ -1,6 +1,13 @@
 # pbp_load.R
 
 # Read data in
+pbp_2014 <- readr::read_csv(
+  here::here("data-raw/pbp_2014.csv"),
+  col_types = c("nnnnccccccncnnncnccnc"))
+
+pbp_2015 <- readr::read_csv(
+  here::here("data-raw/pbp_2015.csv"),
+  col_types = c("nnnnccccccncnnncnccncncccc"))
 
 pbp_2016 <- readr::read_csv(
   here::here("data-raw/pbp_2016.csv"),
@@ -24,42 +31,14 @@ pbp_2020 <- readr::read_csv(
 
 # Tidying
 
-pbp_2016 |>
-  dplyr::mutate(
-    previous_action = NA_real_,
-    qualifier = NA_character_,
-    qualifier1 = NA_character_,
-    qualifier2 = NA_character_,
-    qualifier3 = NA_character_,
-    qualifier4 = NA_character_
-  ) -> pbp_2016_tidy
-
-pbp_2017 |>
-  dplyr::mutate(
-    previous_action = NA_real_,
-    qualifier1 = NA_character_,
-    qualifier2 = NA_character_,
-    qualifier3 = NA_character_,
-    qualifier4 = NA_character_
-  ) -> pbp_2017_tidy
-
-pbp_2018 |>
-  dplyr::mutate(
-    qualifier4 = NA_character_) -> pbp_2018_tidy
-
-pbp_2019 |>
-  dplyr::select(-scoreboard_name) -> pbp_2019_tidy
-
-pbp_2020 |>
-  dplyr::select(
-    -c("clock", "scoreboard_name")) -> pbp_2020_tidy
-
-# Bind rows to merge data across season
 pbp <- dplyr::bind_rows(
-  pbp_2020_tidy,
-  pbp_2019_tidy,
-  pbp_2018_tidy,
-  pbp_2017_tidy,
-  pbp_2016_tidy)
+  pbp_2020,
+  pbp_2019,
+  pbp_2018,
+  pbp_2017,
+  pbp_2016,
+  pbp_2015,
+  pbp_2014) |>
+  dplyr::arrange(desc(season), page_id, action_number)
 
 usethis::use_data(pbp, overwrite = TRUE)
