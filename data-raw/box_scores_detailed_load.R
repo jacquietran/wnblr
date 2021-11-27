ec4f5ac3ebc6ad894d51c2b888f9586a7e209f9a
Jacquie Tran <jacquietran@users.noreply.github.com>
2021-11-28 11:11:49 +1300

    Reload updated data sets

--- data-raw/box_scores_detailed_load.R
+++ data-raw/box_scores_detailed_load.R
@@ -1,36 +1,36 @@
 # box_scores_detailed_load.R
 
-# Read data in
+# Read data in -----------------------------------------------------------------
 
 box_scores_detailed_2014 <- readr::read_csv(
   here::here("data-raw/detailed_box_scores_2014.csv"),
-  col_types = c("nncccccccccnc", rep("c", 27)))
+  col_types = c("nncccccccccncnnnnnnnnnnnnnnnnnnnnnnnnnnnn"))
 
 box_scores_detailed_2015 <- readr::read_csv(
   here::here("data-raw/detailed_box_scores_2015.csv"),
-  col_types = c("nncccccccccnc", rep("c", 27)))
+  col_types = c("nncccccccccncnnnnnnnnnnnnnnnnnnnnnnnnnn"))
 
 box_scores_detailed_2016 <- readr::read_csv(
   here::here("data-raw/detailed_box_scores_2016.csv"),
-  col_types = c("nncccccccccnc", rep("c", 27)))
+  col_types = c("nncccccccccncnnnnnnnnnnnnnnnnnnnnnnnnnnn"))
 
 box_scores_detailed_2017 <- readr::read_csv(
   here::here("data-raw/detailed_box_scores_2017.csv"),
-  col_types = c("nncccccccccnc", rep("c", 27)))
+  col_types = c("nncccccccccncnnnnnnnnnnnnnnnnnnnnnnnnnnn"))
 
 box_scores_detailed_2018 <- readr::read_csv(
   here::here("data-raw/detailed_box_scores_2018.csv"),
-  col_types = c("nncccccccccnc", rep("c", 27)))
+  col_types = c("nncccccccccncnnnnnnnnnnnnnnnnnnnnnnnnnnn"))
 
 box_scores_detailed_2019 <- readr::read_csv(
   here::here("data-raw/detailed_box_scores_2019.csv"),
-  col_types = c("nncccccccccnc", rep("c", 27)))
+  col_types = c("nncccccccccncnnnnnnnnnnnnnnnnnnnnnnnnnnn"))
 
 box_scores_detailed_2020 <- readr::read_csv(
   here::here("data-raw/detailed_box_scores_2020.csv"),
-  col_types = c("nncccccccccnc", rep("c", 27)))
+  col_types = c("nncccccccccncnnnnnnnnnnnnnnnnnnnnnnnnnnn"))
 
-# Tidying
+# Tidying ----------------------------------------------------------------------
 
 box_scores_detailed <- dplyr::bind_rows(
   box_scores_detailed_2020,
@@ -39,6 +39,13 @@ box_scores_detailed <- dplyr::bind_rows(
   box_scores_detailed_2017,
   box_scores_detailed_2016,
   box_scores_detailed_2015,
-  box_scores_detailed_2014)
+  box_scores_detailed_2014) |>
+  dplyr::select(
+    season, page_id, team_name, team_short_name, home_away_flag, team_name_opp,
+    team_short_name_opp, first_name, family_name, scoreboard_name,
+    playing_position, starter, minutes, points, points_second_chance,
+    points_fast_break, points_in_the_paint, points_from_turnovers, everything())
+
+# Create package data ----------------------------------------------------------
 
 usethis::use_data(box_scores_detailed, overwrite = TRUE)
