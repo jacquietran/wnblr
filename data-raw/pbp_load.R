4ea791e83fb13e323fb6d688f2d3390fcdb687d4
Jacquie Tran <jacquietran@users.noreply.github.com>
2021-11-28 02:55:20 +1300

    Tidied up play-by-play data

--- data-raw/pbp_load.R
+++ data-raw/pbp_load.R
@@ -3,34 +3,33 @@
 # Read data in
 pbp_2014 <- readr::read_csv(
   here::here("data-raw/pbp_2014.csv"),
-  col_types = c("nnnnccccccncnnncnccnc"))
+  col_types = c("nnnccccccncnnncccnn"))
 
 pbp_2015 <- readr::read_csv(
   here::here("data-raw/pbp_2015.csv"),
-  col_types = c("nnnnccccccncnnncnccncncccc"))
+  col_types = c("nnnccccccncnnncccnccnn"))
 
 pbp_2016 <- readr::read_csv(
   here::here("data-raw/pbp_2016.csv"),
-  col_types = c("nnnnccccccncnnncnccnncccc"))
+  col_types = c("nnnccccccncnnncccnccnn"))
 
 pbp_2017 <- readr::read_csv(
   here::here("data-raw/pbp_2017.csv"),
-  col_types = c("nnnnccccccncnnncncccnncccc"))
+  col_types = c("nnnccccccncnnncccnccnn"))
 
 pbp_2018 <- readr::read_csv(
   here::here("data-raw/pbp_2018.csv"),
-  col_types = c("nnnnccccccncnnncncccnnccccnccc"))
+  col_types = c("nnnccccccncnnncccnccnnnnnnnnnnnn"))
 
 pbp_2019 <- readr::read_csv(
   here::here("data-raw/pbp_2019.csv"),
-  col_types = c("nnnnnccccccncnnncncccnnccccccccc"))
+  col_types = c("nnnccccccncnnncccnccnnnnnnnnnnnn"))
 
 pbp_2020 <- readr::read_csv(
   here::here("data-raw/pbp_2020.csv"),
-  col_types = c("nnnnnccccccnccnnncncccnnccccccccc"))
+  col_types = c("nnnccccccncnnncccnccnnnnnnnnnnnn"))
 
 # Tidying
-
 pbp <- dplyr::bind_rows(
   pbp_2020,
   pbp_2019,
