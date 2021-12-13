24c5da95c48e090ba9ff24caf0e3a421d4ef693b
Jacquie Tran <jacquie.tran@gmail.com>
2021-12-13 22:29:55 +1300

    Added docs for load_box_scores()

--- R/load_box_scores.R
+++ R/load_box_scores.R
@@ -1,3 +1,10 @@
+#' Load team-level box scores data.
+#'
+#' @return A data set containing all available box scores at the team level, from games played since the 2014/15 season onwards.
+#' @export
+#'
+#' @examples
+#' box_scores_data <- load_box_scores()
 load_box_scores <- function(){
   
   url_string <- "https://github.com/jacquietran/wnblr_data/raw/main/data/box_scores.rds"
